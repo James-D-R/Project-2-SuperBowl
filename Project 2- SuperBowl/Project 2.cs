3506ea21e74f2337752ddfd66749f86feb7d0ebd
remjamd <remjamd@dunwoody.edu>
2018-02-22 21:17:03 -0600

    Part 1 Lists

--- Project 2- SuperBowl/Project 2.cs
+++ Project 2- SuperBowl/Project 2.cs
@@ -133,23 +133,77 @@ namespace Project_2__SuperBowl
                 ListState = ListState.Concat(o).ToArray();
             }
 
-            SuperBowl[] SuperBowlObjects = new SuperBowl[51]; //Create a list of Superbowl objects
-            for (int x = 0; x < 51; x++)
-            {
-                
-                SuperBowlObjects[x] = new SuperBowl(x, ListDate, ListSB, ListAttendance, ListQBWinner, ListCoachWinner, ListTeamWinner, ListWinningPts, ListQBLoser,
+            SuperBowl[] SuperBowlObjects = CreateObjects(ListDate, ListSB, ListAttendance, ListQBWinner, ListCoachWinner, ListTeamWinner, ListWinningPts, ListQBLoser,
                             ListCoachLoser, ListTeamLoser, ListLosingPts, ListMVP, ListStadium, ListCity, ListState);
-            };
-            Console.WriteLine(SuperBowlObjects[1].SB);
+
+            string[,] WinningStats = SuperBowlWinners(SuperBowlObjects); // Returns muli-dimensional array of SuperBowl winner's stats
+            
+            for (var i = 0; i < 51; i++)
+            {
+                Console.WriteLine("1.{0}", WinningStats[i, 0]);
+                Console.WriteLine("2.{0}", WinningStats[i, 1]);
+                Console.WriteLine("3.{0}", WinningStats[i, 2]);
+                Console.WriteLine("4.{0}", WinningStats[i, 3]);
+                Console.WriteLine("5.{0}", WinningStats[i, 4]);
+                Console.WriteLine("6.{0}", WinningStats[i, 5]);
+                Console.WriteLine();
+
+            }
+            
+            //Console.WriteLine(SuperBowlObjects[1].SB);
             Console.ReadKey();
 
 
-        }
+        }// End of Main Method
+
         static StreamReader OpenFile()
         {
             FileStream file = new FileStream("Super_Bowl_Project.csv", FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(file);
             return reader;
-        }
+        }// End of OpenFile Method
+
+        static SuperBowl[] CreateObjects(string[] ListDate, string[] ListSB, int[] ListAttendance, string[] ListQBWinner,
+                                         string[] ListCoachWinner, string[] ListTeamWinner, int[] ListWinningPts, string[] ListQBLoser,
+                                         string[] ListCoachLoser, string[] ListTeamLoser, int[] ListLosingPts, string[] ListMVP,
+                                         string[] ListStadium, string[] ListCity, string[] ListState)
+        {
+            SuperBowl[] SuperBowlObjects = new SuperBowl[51]; //Create a list of Superbowl objects
+            for (int x = 0; x < 51; x++)
+            {
+
+                SuperBowlObjects[x] = new SuperBowl(x, ListDate, ListSB, ListAttendance, ListQBWinner, ListCoachWinner, ListTeamWinner, ListWinningPts, ListQBLoser,
+                            ListCoachLoser, ListTeamLoser, ListLosingPts, ListMVP, ListStadium, ListCity, ListState);
+            };
+            return SuperBowlObjects;
+        }// End of CreateObjects Method
+
+        static string[,] SuperBowlWinners(SuperBowl[] SuperBowlObjects)
+        {
+            string[,] WinningStats = new string[51, 6];
+            for (int x = 0; x < SuperBowlObjects.Length; x++)
+            {
+                string year = SuperBowlObjects[x].Date;
+                int yearLength = year.Length;
+                int last = yearLength - 1;
+                int first = yearLength - 2;
+                string firstStr = year[first].ToString();
+                string lastStr = year[last].ToString();
+                string yearAbrv = "'" + firstStr + lastStr;
+                int PointDif = SuperBowlObjects[x].WinningPts - SuperBowlObjects[x].LosingPts;
+                string PointDifStr = PointDif.ToString();
+
+                string[] Single = {SuperBowlObjects[x].WinningTeam, yearAbrv, SuperBowlObjects[x].QBWinner, SuperBowlObjects[x].CoachWinner,SuperBowlObjects[x].MVP, PointDifStr};
+                WinningStats[x, 0] = SuperBowlObjects[x].WinningTeam;
+                WinningStats[x, 1] = yearAbrv;
+                WinningStats[x, 2] = SuperBowlObjects[x].QBWinner;
+                WinningStats[x, 3] = SuperBowlObjects[x].CoachWinner;
+                WinningStats[x, 4] = SuperBowlObjects[x].MVP;
+                WinningStats[x, 5] = PointDifStr;
+
+                
+            }
+            return WinningStats;
+        }// End of SuperBowlWinners Method
     }
 }
